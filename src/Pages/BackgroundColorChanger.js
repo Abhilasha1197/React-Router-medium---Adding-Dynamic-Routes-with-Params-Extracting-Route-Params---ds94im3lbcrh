a4ca1b7298cc13f304d3dec99c2cd7d429cad02c
abhilasha <abhilasha15597@gmail.com>
2022-12-22 17:13:02 +0530

    new

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 
 export const BackgroundColorChanger = () =>{
+    const param = useParams();
     return(
-       
+       <main style={{height:'100vh',backgroundColor:param.colorname}}>
+       </main>
     )
 }
--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 
 export const Calculator = () =>{
-
+    const  [qr] = useSearchParams();
+    const n1 = Number(qr.get('num1'));
+    const n2 = Number(qr.get('num2'));
+    const op = qr.get('op');
+    // console.log(n1,n2,op);
+    let result = eval(`${n1}${op}${n2}`);
     return(
-        
+        <div>Your calculation Result is <span id="calc-result">{result}</span></div>
     )
 }
